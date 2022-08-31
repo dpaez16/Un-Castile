cf2f8e3d34d2f69487e9532c5da947548fa0587f
Danny Paez <dpaez16@yahoo.com>
2022-08-30 17:06:34 -0700

    setValue method

--- Frontend/src/Classes/UnCastilePlayer.js
+++ Frontend/src/Classes/UnCastilePlayer.js
@@ -10,6 +10,15 @@ class UnCastilePlayer {
     getMetadata() {
         return this.metadata;
     }
+
+    /**
+     * Sets a value in the player's metadata.
+     * @param {string} key - The key for associated value
+     * @param {any} value - The value for the associated key
+     */
+    setValue(key, value) {
+        this.metadata[key] = value;
+    }
 };
 
 module.exports = { UnCastilePlayer };
